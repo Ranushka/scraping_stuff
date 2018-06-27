5f1744722ccf0d30f52678d39c9412282cd19a59
ranu <ranu@sprii.com>
2018-06-27 15:35:53 +0400

    optimise babyshopstors

--- jobs/getProducts/babyshopstores_products.js
+++ jobs/getProducts/babyshopstores_products.js
@@ -1,100 +1,50 @@
 'use strict';
 
-const Xvfb = require('xvfb');
 const Nightmare = require('nightmare');
-const fetch = require('node-fetch');
+const nightmare = Nightmare();
 const vo = require('vo');
-const pm2 = require('pm2');
-
-const xvfb = new Xvfb({
-  silent: true
-});
-
-const nightmare = Nightmare({
-  show: false
-});
-
-xvfb.startSync();
-
+const lib = require('../../lib');
 const siteName = "babyshopstores";
 
-var nextExists = true;
+lib.start(siteName, getProductLinks);
 
-vo(start)(function (err, result) {
-  if (err) throw err;
-});
+async function getProductLinks(gotoUrl) {
 
-function* start() {
+  await nightmare.goto(gotoUrl);
 
-  let i = 0;
-  let nextMainLink = true;
+  var haveMore = true;
 
-  console.log('kicking off');
+  //
+  // ─── GO WHILE SCRAPING COMPLEAT THE PAGINATION ON THE LINK A LINK ───────────────
+  //
+  while (haveMore) {
 
-  while (nextMainLink) {
-    let link, urlToScrap;
-    urlToScrap = yield getNewScrapURL();
-
-    console.log('nextMainLink --- ', nextMainLink)
-
-    if (urlToScrap.greeting) {
-      nextExists = true;
-      yield getProductLinks(urlToScrap.greeting.url);
-    } else {
-      nextMainLink = false;
-    }
-
-    i++;
-  }
-
-  xvfb.stopSync();
-  pm2.killDaemon();
-  console.log(`${siteName} done save data ${i}`);
-
-}
-
-function* getNewScrapURL() {
-  return yield fetch(`https://sitedata-mum.herokuapp.com/api/links/nextScrapLink?site=${siteName}`, {
-      method: 'GET'
-    }).then(res => res.json())
-    .then(json => {
-      return json;
-    });
-}
-
-function* getProductLinks(catLists) {
-
-  var nightmare = Nightmare(),
-    gotoUrl = catLists,
-    nextExists = true,
-    allLinks = [];
-
-  let scrapedData = yield nightmare
-    .goto(gotoUrl)
-
-
-  while (nextExists) {
-
-    // loging scraping site url
-    var url = yield nightmare.url();
+    //
+    // ─── LOGING SCRAPING SITE URL ────────────────────────────────────
+    //
+    var url = await nightmare.url();
     console.log(url);
 
-    yield nightmare
+    await nightmare
       .wait(4000)
       .evaluate(function () {
 
         var links = [],
           productList = document.querySelectorAll('.product-item'),
-          nextExists = document.querySelectorAll('[class="next"]').length ? true : false,
+          pagination = document.querySelectorAll('[class="next"]').length ? true : false,
           tagsElemnts = document.querySelectorAll('.breadcrumb > li > a'),
           tags = [];
 
-        // get tags for the product
+        //
+        // GET TAGS FOR THE PRODUCT
+        //
         tagsElemnts.forEach(function (a) {
           tags.push(a.innerText)
         });
 
-        // going through each product
+        //
+        // GOING THROUGH EACH PRODUCT
+        //
         productList.forEach(function (item) {
           links.push({
             "name": item.querySelectorAll('[itemprop="name"]')[0].innerText.trim(),
@@ -105,41 +55,26 @@ function* getProductLinks(catLists) {
           });
         });
 
-        // return all the values
+        //
+        // RETURN ALL THE VALUES
+        //
         return {
           'links': links,
-          'nextExists': nextExists
+          'pagination': pagination
         };
 
-      }).then(async function (resalt) {
-        nextExists = resalt.nextExists;
-        await saveToDb(resalt.links);
+      }).then(function (resalt) {
+        haveMore = resalt.pagination;
+        lib.PrepToSave(resalt.links);
       })
 
-    if (nextExists) {
-      yield nightmare
+    //
+    // ─── CHECK HAVE MORE TO THE PAGE ─────────────────────────────────
+    //  
+    if (haveMore) {
+      await nightmare
         .click('.next')
         .wait(2000)
     }
   }
-
-  yield nightmare.end();
-
-}
-
-function* saveToDb(params) {
-
-  var jsonData = JSON.stringify(params);
-  return yield fetch('https://sitedata-mum.herokuapp.com/api/products', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: jsonData
-    })
-    .then(res => res.json())
-    .then(json => {
-      console.log('data saved')
-    });
-
 }
