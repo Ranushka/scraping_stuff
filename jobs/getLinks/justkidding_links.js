4e4d2d3a682f85267d834ceee7adc39fd897e8cd
ranu <ranu@sprii.com>
2018-07-03 16:25:38 +0400

    add jutkidding

---
+++ jobs/getLinks/justkidding_links.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Nightmare = require('nightmare');
+const fetch = require('node-fetch');
+const vo = require('vo');
+const lib = require('../../lib');
+
+console.log(`get main links start`);
+
+var links = [
+  "https://www.justkidding-me.com/"
+];
+
+start();
+
+async function start() {
+  for (let link of links) {
+    await getMultipalSoursLinks(link)
+  }
+}
+
+async function getMultipalSoursLinks(urlToScrape) {
+  console.log('get main links', urlToScrape);
+  var nightmare = Nightmare();
+
+  await nightmare
+    .goto(`${urlToScrape}`)
+    .wait(2000)
+    .evaluate(function () {
+      var brandPageList = document.querySelectorAll('#queldoreiNav ol > li > a');
+      var brandPageLinks = [];
+      brandPageList.forEach(function (item) {
+        brandPageLinks.push({
+          "name": item.innerText.trim(),
+          "url": item.href,
+          "site": "justkidding",
+          "scrap": false
+        });
+      });
+
+      return brandPageLinks;
+    })
+    .then(function (result) {
+      lib.PrepToSave(result, `${lib.APIbaseUrl}/api/links/createOrUpdate`, urlToScrape);
+    })
+    .catch(function (error) {
+      console.error(`Error - `, error);
+    });
+}
