7fca6606e778f5f3ebc13db809cedf91366de49b
ranu <ranu@sprii.com>
2018-07-03 10:58:31 +0400

    update mums links

--- jobs/getLinks/mumzworld_links.js
+++ jobs/getLinks/mumzworld_links.js
@@ -5,79 +5,46 @@ const fetch = require('node-fetch');
 const vo = require('vo');
 const lib = require('../../lib');
 
-
 console.log(`get main links start`);
 
-var aaa = [
+var links = [
   "http://www.mumzworld.com/en/"
 ];
 
-vo(start)(function (err, result) {
-  if (err) throw err;
-});
+start();
 
-function* start() {
-  var i = 0;
-  while (i < aaa.length) {
-    yield getMultipalSoursLinks(aaa[i])
-    i++
+async function start() {
+  for (let link of links) {
+    console.log(link)
+    await getMultipalSoursLinks(link)
   }
 }
 
-async function getMultipalSoursLinks(params) {
-  console.log('get main links', params)
+async function getMultipalSoursLinks(urlToScrape) {
+  console.log('get main links', urlToScrape);
   var nightmare = Nightmare();
 
   await nightmare
-    .goto(`${params}`)
+    .goto(`${urlToScrape}`)
     .wait(2000)
     .evaluate(function () {
-      // get only baby prducts
-      // var brandPageList = document.querySelectorAll('.brands a.box');
       var brandPageList = document.querySelectorAll('#custommenu h2.level2title a'),
-          brandPageLinks = [];
-
+        brandPageLinks = [];
 
       brandPageList.forEach(function (item) {
-
         brandPageLinks.push({
           "name": item.innerText.trim(),
           "url": item.href,
           "site": "mumzworld",
           "scrap": false
         });
-
       });
       return brandPageLinks;
     })
-    .then(async function (result) {
-      await lib.PrepToSave(result, `${lib.APIbaseUrl}/api/links`);
+    .then(function (result) {
+      lib.PrepToSave(result, `${lib.APIbaseUrl}/api/links/createOrUpdate`, urlToScrape);
     })
     .catch(function (error) {
-      console.error('Error:', error);
+      console.error(`Error - `, error);
     });
-
 }
-
-// function* prepToSave(catLists) {
-//   var i, j, temparray = [],
-//     chunk = 100;
-//   for (i = 0, j = catLists.length; i < j; i += chunk) {
-//     yield saveToDb(catLists.slice(i, i + chunk));
-//   }
-// }
-
-// function* saveToDb(params) {
-//   console.log(params)
-//   return yield fetch(`${lib.APIbaseUrl}/api/links`, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(params)
-//     })
-//     .then(res => res.json())
-//     .then(json => {
-//       console.log(json)
-//     });
-// }
