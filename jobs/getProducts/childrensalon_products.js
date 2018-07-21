e079625ab8f794048e2b89da914ae5263cf2804b
ranu <ranu@sprii.com>
2018-07-21 16:07:32 +0400

    childrensalon adding product collector

---
+++ jobs/getProducts/childrensalon_products.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const Nightmare = require('nightmare');
+const lib = require('../../lib');
+const logger = require('../../logger');
+const siteName = "childrensalon";
+
+lib.start(siteName, getProductLinks);
+
+async function getProductLinks(urlToScrape) {
+  let haveMore = true,
+    nightmare = new Nightmare();
+
+  console.log('start scraping init', urlToScrape);
+
+  /** 
+   * visiting init page url */
+  await nightmare
+    .goto(urlToScrape)
+    .wait(lib.waitTime)
+    .catch(error => {
+      haveMore = false;
+      console.error('Error start scraping init', urlToScrape, error);
+    })
+
+  /** 
+   * lopp until pagination false */
+  while (haveMore) {
+
+    /** 
+     * now scraping url */
+    var url = await nightmare.url();
+    console.log(url);
+
+    /** 
+     * Start collecting product Data */
+    await nightmare
+      .wait(lib.waitTime)
+      .evaluate(function () {
+
+        var links = [],
+          productList = document.querySelectorAll('.products-grid li.product-item:not(.empty-item)'),
+          pagination = Boolean(document.querySelectorAll('.pages li.next a').length);
+
+        /** 
+         * going through each product */
+        productList.forEach(function (item) {
+          links.push({
+            "name": item.getElementsByClassName('product-item-link')[0].innerText.trim(),
+            "url": item.querySelectorAll('.product-name a')[0].href,
+            "price": item.querySelectorAll('.price')[0].innerText.replace('AED', '').trim(),
+            "brand": item.querySelectorAll('.designer')[0].innerText.trim(),
+            "site": "childrensalon",
+          })
+        })
+
+        /** 
+         * return all the values */
+        return {
+          'links': links,
+          'pagination': pagination
+        };
+
+      })
+      .then(async result => {
+        haveMore = result.pagination;
+        await lib.PrepToSave(result.links, `${lib.APIbaseUrl}/api/products/createOrUpdate`, urlToScrape);
+      })
+      .catch(error => {
+        haveMore = false;
+        logger.error(`init_scraping_fail | ${url} | ${error}`)
+      })
+
+    /** 
+     * Paginating if avalable */
+    if (haveMore) {
+      await nightmare
+        .evaluate(function () {
+          window.location = document.querySelectorAll('.pages li.next a')[0].href;
+        })
+        .catch(error => {
+          haveMore = false;
+          logger.error(`paginating_error | ${url} | ${error}`)
+        })
+    }
+  }
+
+  /** 
+   * nightmare kill */
+  console.log('kill nightmare - ', urlToScrape);
+  await nightmare.end();
+}
